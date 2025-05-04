2def26244bfaeab04286a72d494dcfa7bf19172c
Franco Chan <kinfaifrancochan@gmail.com>
2025-05-04 14:53:47 +0100

    Add agent tools, to get username and current date time

--- ui_components/sidebar.py
+++ ui_components/sidebar.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
 def sidebar():
+  if "username" not in st.session_state:
+    st.session_state.username = None
+
   with st.sidebar:
     if not st.user.is_logged_in:
       st.button("Log in with Google", on_click=st.login)
+      st.session_state.username = None
     else:
+      st.session_state.username = st.user.name
       st.button("Log out", on_click=st.logout)
       st.image(st.user.picture)
       st.write(st.user.name)
